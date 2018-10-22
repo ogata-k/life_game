d662ecc3335a8c16c8676864422372ed255c9ae3
ogata-k <ogtkzk712@gmail.com>
2018-10-22 18:09:43 +0900

    first base state

---
+++ src/bin/lifegame.rs
@@ -0,0 +1,9 @@
+extern crate life_game;
+
+use life_game::game::status;
+use life_game::game::stage;
+use life_game::game::next;
+
+fn main() {
+    println!("start life game.");
+}
---
+++ src/game/mod.rs
@@ -0,0 +1,4 @@
+pub mod status;
+pub mod stage;
+pub mod next;
+
