f8b969fa5ad2da9bfe671481b3e32caf6b8a1945
ogata-k <ogtkzk712@gmail.com>
2018-10-29 16:05:29 +0900

    impl nomal lifegame

--- src/bin/lifegame.rs
+++ src/bin/lifegame.rs
@@ -1,44 +1,58 @@
+/// このライフゲームは周辺の状態を利用して自身の状態を書き換えるタイプのライフゲーム
+/// となります。そのためエージェントが移動するタイプのライフゲームには使えません。
+///
+
 extern crate life_game;
 
-use life_game::game::status;
 use life_game::game::stage;
-use life_game::game::next;
+use life_game::game::stage::Stage;
 
 // 生存時間定数の定義
 //（できればサイズなどの初期化パラメータや時間などのパラメータはオプションで指定できるようにしたい）
 
 
 fn main() {
-    let TIME = 100;
+    let TIME = 4;
     // 初期状態を生成して保持状態に（例：newで呼び出したあとにrandomに渡すことでランダム化）
-    let mut state = stage::Stage::make_stage(false, 300, 400);
+    let mut stage = stage::Stage::make_stage(false, 4, 4);
+    stage.set_data(&1, &1, &true);
+    stage.set_data(&1, &2, &true);
+    stage.set_data(&2, &1, &true);
+    stage.set_data(&2, &2, &true);
 
     // 初期状態を画像として書き出す（例：lifegame_0.png）
-    println!("{:?}", state);
+    println!("{:?}", stage);
 
     // 生存時間の間だけwhileループ
     for t in 1 .. TIME+1 {
         // 前回の状態をコピーして用意
-        let mut new_state = state.copy();
 
-        /*
-        // イテレータからデータ情報と番号を取得
-        for (ref i, ref j, ref s) in state{
-            // スライスの各データ毎に
-            // データと番号から周辺のデータ情報を取得
-            let nbh: [u16] = &state.get_nhd(&i);
-            // データ情報とそのデータ情報から次の状態を計算
-            let data = calc_status(&state, nbh, &i);
-            // コピーして用意しておいた情報に結果を保存
-            new_state.set_data(&i, data);
-        }
-        */
-        // 結果を保持状態に保持
-        state = new_state.copy();
+        // 次のステージを計算して結果を更新
+        stage = stage.calc_stage(nbh_rule, calc_rule);
 
         // 保持状態を画像として書き出す
-        println!("{:?}", state);
+        println!("{:?}", stage);
     }
 
     // 画像をGifに変換
+}
+
+fn nbh_rule(s: &Stage<bool>, i: usize, j: usize) -> Vec<(usize, usize)>{
+    let (h, w) = (*s).get_sizes();
+    return vec![
+        ((i+h-1)%h, (j+w-1)%w), ((i+h-1)%h, j), ((i+h-1)%h, (j+1)%w),
+        (i, (j+w-1)%w), (i, (j+1)%w),
+        ((i+1)%h, (j+w-1)%w), ((i+1)%h, j), ((i+1)%h, (j+1)%w)
+    ];
+}
+
+fn calc_rule(s: &Stage<bool>, i: usize, j: usize, data: bool, nbh: Vec<(usize, usize)>) -> bool{
+    let v: Vec<_> = nbh.iter().map(|index| {*(*s).get_data(&index.0, &index.1)}).filter(|t| *t).collect();
+    let c = v.len();
+    // 生きているセルをtrueとして扱うことにする
+    return if data{
+        if c==2 || c==3 {true}else {false}
+    }else{
+        if c==3 {true}else{false}
+    };
 }
